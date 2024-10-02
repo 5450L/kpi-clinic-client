c6436677e426dd2c0a2eae3be1c73ca0236da93d
Alinesa <alinesa322@gmail.com>
2024-10-02 18:57:17 +0300

    add

--- src/app/components/main-page/team-components/member-list/member-list.component.ts
+++ src/app/components/main-page/team-components/member-list/member-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
-import { MemberItemComponent } from "../member-item/member-item.component";
+import { MemberItemComponent } from '../member-item/member-item.component';
 import { Member } from '../../../../models/member.model';
 import { TeamService } from '../../../../services/team/team.service';
 @Component({
@@ -8,13 +8,12 @@ import { TeamService } from '../../../../services/team/team.service';
   imports: [MemberItemComponent],
   templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.css',
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class MemberListComponent implements OnInit {
+  constructor(private teamService: TeamService) {}
 
-  constructor(private teamService: TeamService) { }
-
-  memberList: Member[] = []
+  memberList: Member[] = [];
 
   breakpoints = {
     1: {
@@ -23,12 +22,15 @@ export class MemberListComponent implements OnInit {
     640: {
       slidesPerView: 2,
     },
-    1024: {
+    768: {
       slidesPerView: 3,
-    }
-  }
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+  };
 
   ngOnInit() {
-    this.memberList = this.teamService.getMembers()
+    this.memberList = this.teamService.getMembers();
   }
 }
