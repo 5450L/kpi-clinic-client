4f711a4fd2f87b1d7a13d82007ed62a90aaa21cb
5450L <susalafamilia@gmail.com>
2024-11-08 21:47:44 +0200

    header+ title

--- src/app/components/main-page/main-page-section/main-page-section.component.ts
+++ src/app/components/main-page/main-page-section/main-page-section.component.ts
@@ -1,13 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'main-page-section',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './main-page-section.component.html',
-  styleUrl: './main-page-section.component.css'
+  styleUrl: './main-page-section.component.css',
 })
 export class MainPageSectionComponent {
   @Input() title: string = '';
-
 }
